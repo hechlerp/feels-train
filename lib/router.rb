e7e5f157bf9f574dc4ae1a392d3960567f9708e0
Peter Hechler <peter.hechler@gmail.com>
2016-04-18 09:19:10 -0400

    Prepared main files

--- lib/router.rb
+++ lib/router.rb
@@ -8,14 +8,11 @@ class Route
     @action_name = action_name
   end
 
-  # checks if pattern matches path and method matches request method
   def matches?(req)
     @pattern =~ req.path && req.request_method
       .downcase.to_sym == @http_method
   end
 
-  # use pattern to pull out route params (save for later?)
-  # instantiate controller and call controller action
   def run(req, res)
     path = Regexp.new @pattern
     matched = path.match(req.path)
@@ -36,28 +33,22 @@ class Router
     @routes = []
   end
 
-  # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
     route = Route.new(pattern, method, controller_class, action_name)
     @routes << route
   end
 
-  # evaluate the proc in the context of the instance
-  # for syntactic sugar :)
   def draw(&proc)
     self.instance_eval(&proc)
 
   end
 
-  # make each of these methods that
-  # when called add route
   [:get, :post, :put, :delete].each do |http_method|
     define_method(http_method) do |pattern, controller_class, action_name|
       add_route(pattern, http_method, controller_class, action_name)
     end
   end
 
-  # should return the route that matches this request
   def match(req)
     @routes.each do |route|
       return route if route.matches?(req)
@@ -65,7 +56,6 @@ class Router
     nil
   end
 
-  # either throw 404 or call run on a matched route
   def run(req, res)
     route = match(req)
     if route
--- lib/session.rb
+++ lib/session.rb
@@ -1,9 +1,6 @@
-require 'byebug'
 require 'json'
 
 class Session
-  # find the cookie for this app
-  # deserialize the cookie into a hash
   def initialize(req)
     if req.cookies["_rails_lite_app"].nil?
       @session = {}
@@ -21,8 +18,6 @@ class Session
     @session[key] = val
   end
 
-  # serialize the hash into json and save in a cookie
-  # add to the responses cookies
   def store_session(res)
     res.set_cookie("_rails_lite_app",{path: "/", value: @session.to_json})
 
